84c7ed14e6e30e663b30990c6786be1e8d66dc2e
Tom Apol <tom142606@gmail.com>
2018-09-28 01:51:02 +0200

    Cleaned up code and added right plot labels

--- Lab3/code/lab3_1.m
+++ Lab3/code/lab3_1.m
@@ -1,7 +1,10 @@
 close all;
 
-load("../lab_week3_data/lab3_1.mat")
+load('../lab_week3_data/lab3_1.mat')
 
+%========================== 1.1 ================================%
+% compute posterior probabilities
+% occurences of sea bass vs salmon is 3:1
 a_priori_seabass = .75;
 a_priori_salmon = .25;
 
@@ -10,23 +13,26 @@ evidence = p_seabass * a_priori_seabass + p_salmon * a_priori_salmon;
 posterior_seabass = p_seabass * a_priori_seabass ./ evidence;
 posterior_salmon = p_salmon * a_priori_salmon ./ evidence;
 
+% plot figure
 figure; hold on;
 plot(l, posterior_seabass, 'r-');
 plot(l, posterior_salmon, 'b-');
-xlabel("length (cm)");
-ylabel("posterior probability");
-title("Posterior probabilities of seabass and salmon");
-legend("seabass", "salmon");
+xlabel('length (cm)');
+ylabel('posterior probability');
+title('Posterior probabilities of sea bass and salmon');
+legend('seabass', 'salmon');
 
-%===============================================================%
+%========================== 1.2 ================================%
+% get posterior probabilities for lengths 8 and 20
+l_8_index = 71;
+l_20_index = 191;
+pos_sb_8 = posterior_seabass(l_8_index)
+pos_sal_8 = posterior_salmon(l_8_index)
+pos_sb_20 = posterior_seabass(l_20_index)
+pos_sal_20 = posterior_salmon(l_20_index)
 
-l_8 = 71;
-l_20 = 191;
-pos_sb_8 = posterior_seabass(l_8)
-pos_sal_8 = posterior_salmon(l_8)
-pos_sb_20 = posterior_seabass(l_20)
-pos_sal_20 = posterior_salmon(l_20)
 
+% plot figure
 figure; hold on;
 x_vals_8 = zeros(21)+8;
 x_vals_20 = zeros(21)+20;
@@ -37,7 +43,7 @@ plot(l, posterior_salmon, 'b-');
 plot(x_vals_8, y_vals, 'k-');
 plot(x_vals_20, y_vals, 'k--');
 
-xlabel("length (cm)");
-ylabel("posterior probability");
-title("Posterior probabilities of seabass and salmon");
-legend("seabass", "salmon", "fish");
+xlabel('length (cm)');
+ylabel('posterior probability');
+title('Posterior probabilities of sea bass and salmon');
+legend('sea bass', 'salmon', 'fish');
--- Lab3/code/lab3_2.m
+++ Lab3/code/lab3_2.m
@@ -1,23 +1,28 @@
 close all;
 
-load("../lab_week3_data/normdist.mat");
+load('../lab_week3_data/normdist.mat');
 
+%========================== 2.1 ================================%
+% create plot
 figure; hold on;
 plot(S1, zeros(30), 'bo');
 plot(S2, zeros(15)+1, 'ro');
 plot(T, zeros(5)+2, 'ks');
+% Remove unnecessary y-axis ticks.
 set(gca,'YTickLabel',[]);
 set(gca,'YTick',[]);
-title("Set 1, Set 2, Test set");
-legend("seabass", "salmon", "fish");
-%===========================================================%
+title('Set 1, Set 2, Test set');
+legend('Set 1', 'Set 2', 'Test set');
 
+%========================== 2.2 ================================%
+% compute mean and standard deviation of S1 and S2
 mu_hat_S1 = sum(S1) / length(S1);
 sigma_hat_S1 = sqrt(sum((S1-mu_hat_S1).*(S1-mu_hat_S1)) / length(S1));
 
 mu_hat_S2 = sum(S2) / length(S2);
 sigma_hat_S2 = sqrt(sum((S2-mu_hat_S2).*(S2-mu_hat_S2)) / length(S2));
 
+% create and plot the normal distributions
 x_values = -40:.1:100;
 
 pdf_S1 = normpdf(x_values, mu_hat_S1, sigma_hat_S1);
@@ -28,52 +33,68 @@ plot(x_values, pdf_S1, 'b-');
 plot(x_values, pdf_S2, 'r-');
 plot(S1, zeros(30), 'bo');
 plot(S2, zeros(15), 'ro');
-xlabel("length (cm)");
-ylabel("posterior probability");
-title("Posterior probabilities of seabass and salmon");
-legend("seabass", "salmon", "fish");
-%============================================================%
+ylabel('class conditional probability density');
+title('Class conditional probability densities of Set 1 and Set 2');
+legend('CCPD S_1', 'CCPD S_1', 'S_1', 'S_2');
 
+%========================== 2.3 ================================%
+% compute a priori probabilities
 prior_S1 = length(S1)/(length(S1)+length(S2))
 prior_S2 = length(S2)/(length(S1)+length(S2))
 
-%============================================================%
-
+%========================== 2.4 ================================%
+% create plot
 figure; hold on;
 plot(x_values, prior_S1*pdf_S1, 'b-');
 plot(x_values, prior_S2*pdf_S2, 'r-');
 plot(S1, zeros(30), 'bo');
 plot(S2, zeros(15), 'ro');
-plot(T, zeros(5), 'ks');
+plot(T, zeros(5)+0.007, 'ks');
+%ylabel('P(\omega_i)p(x|\omega_i)');
+%title('P(\omega_1)p(x|\omega_1), P(\omega_2)p(x|\omega_2), S1, S2 and T');
+ylabel('non-normalised posterior probability');
+title(['Non-normalised posterior probabilities with' ...
+  ' original and Test data']);
+legend('NNPP S_1', 'NNPP S_2', 'S_1', 'S_2', 'T');
 
-%===========================2.5=================================%
+%========================== 2.5 ================================%
 
 % manual calculation
 a = sigma_hat_S1 * sigma_hat_S1 - sigma_hat_S2 * sigma_hat_S2;
-b = 2*sigma_hat_S2 * sigma_hat_S2 * mu_hat_S1 - 2 * sigma_hat_S1 *...
-    sigma_hat_S1 * mu_hat_S2;
+b = 2*(sigma_hat_S2 * sigma_hat_S2 * mu_hat_S1 - ...
+    sigma_hat_S1 * sigma_hat_S1 * mu_hat_S2);
 c = sigma_hat_S1 * sigma_hat_S1 * mu_hat_S2 * mu_hat_S2 - ...
     sigma_hat_S2 * sigma_hat_S2 * mu_hat_S1 * mu_hat_S1 + ...
     2 * sigma_hat_S1 * sigma_hat_S1 * sigma_hat_S2 * sigma_hat_S2 * ...
     log(prior_S1 * sigma_hat_S2 / (prior_S2 * sigma_hat_S1));
 
 D = b*b - 4*a*c;
-x1 = -b + sqrt(D) / (2*a);
-x2 = -b - sqrt(D) / (2*a);
 
-%matlab calculation
+% Assuming D is non-negative and we won't have complex answers,
+% sqrt(D) is always non-negative, so x1 <= x2.
+x1 = (-b - sqrt(D)) / (2*a);
+x2 = (-b + sqrt(D)) / (2*a);
+
+% matlab calculation
 syms 'x';
-expression = solve(prior_S1 * sigma_hat_S2* ...
-    exp(-(x-mu_hat_S1)*(x-mu_hat_S1)/(2*sigma_hat_S1*sigma_hat_S1)) == ...
+expression = solve( ...
+    prior_S1 * sigma_hat_S2* ...
+    exp(-(x-mu_hat_S1)*(x-mu_hat_S1)/(2*sigma_hat_S1*sigma_hat_S1)) ...
+    == ...
     prior_S2 * sigma_hat_S1* ...
     exp(-(x-mu_hat_S2)*(x-mu_hat_S2)/(2*sigma_hat_S2*sigma_hat_S2)) ...
     );
-x = eval(expression);
-x_1 = x(1);
-x_2 = x(2);
+x_eval = eval(expression);
+if x_eval(1) < x_eval(2)  % ensure x_1 <= x_2
+  x_1 = x_eval(1);
+  x_2 = x_eval(2);
+else
+  x_1 = x_eval(2);
+  x_2 = x_eval(1);
+end
 
-%===========================2.6=================================%
-
+%========================== 2.6 ================================%
+% create plot
 figure; hold on;
 plot(x_values, prior_S1*pdf_S1, 'b-');
 plot(x_values, prior_S2*pdf_S2, 'r-');
@@ -82,14 +103,17 @@ plot(S2, zeros(15), 'ro');
 plot(T, zeros(5)+0.007, 'ks');
 plot([x_1 x_1], [0 0.014], 'k-');
 plot([x_2 x_2], [0 0.014], 'k-');
-%posterior_S1 = 
+ylabel('non-normalised posterior probability');
+title(['Non-normalised posterior probabilities with' 10 ...
+'original data, Test data, and decision criterions']);
+legend('NNPP S_1', 'NNPP S_2', 'S_1', 'S_2', 'T', 'DC');
 
-%===========================2.7=================================%
+%========================== 2.7 ================================%
 %error rate S1
 omega_1_error_a = normcdf(x_1, mu_hat_S2, sigma_hat_S2);
 omega_1_error_b = 1 - normcdf(x_2, mu_hat_S2, sigma_hat_S2);
-omega_1_error = omega_1_error_a + omega_1_error_b;
+omega_1_error = omega_1_error_a + omega_1_error_b
 
 %error rate S2
 omega_2_error = normcdf(x_2, mu_hat_S1, sigma_hat_S1) - ...
-    normcdf(x_1, mu_hat_S1, sigma_hat_S1);
+    normcdf(x_1, mu_hat_S1, sigma_hat_S1)
