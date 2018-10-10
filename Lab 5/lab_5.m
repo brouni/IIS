3dd22df1319cacf7e65bfadaf54f3c780b4e49b8
Tom Apol <tom142606@gmail.com>
2018-10-10 13:52:51 +0200

    Added core of lab_5

--- Lab 5/lab_5.m
+++ Lab 5/lab_5.m
@@ -13,7 +13,7 @@ data = data_x; % replace with dataset of your choice.
 % Initialise variables.
 [P, N] = size(data);
 K = 2;
-learning_rate = 0.1;
+learning_rate = 0.1; % eta
 t_max = 1000;
 
 % Initialise prototypes
@@ -23,4 +23,36 @@ for i = 1:K
 end
 
 % Permutations
-%  Shuffle data set
+for i = 1:t_max
+  H_VQ = 0; % initialise quantisation error for manual sum.
+  
+  %  Shuffle data set and perform permutations
+  for j = randperm(P)
+    example = data(j, :);
+    
+    % initialise winner
+    winner = 1;
+    winner_distance = intmax; % represents infinity
+    
+    % get closest prototype
+    for k = 1:K % take rows
+      prototype = prototypes(k, :);
+      distance = pdist([example; prototype]);
+      if distance < winner_distance
+        winner_distance = distance;
+        winner = k;
+      end
+    end
+    
+    % update winner
+    prototype(winner) = prototype(winner) + ...
+      learning_rate * (example - prototype(winner));
+  
+    % update (partial) sum of quantisation error
+    H_VQ = H_VQ + winner_distance;
+  end
+  
+  % TODO: plot data and prototype positions, perhaps outside of this loop
+  
+  % TODO: plot quantisation error H_VQ, perhaps outside of this loop
+end
